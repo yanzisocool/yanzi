0ab0eb0c57ec4f4bdacf9b2cfc022cc7a7ec0ee6
yanzisocool <130196378+yanzisocool@users.noreply.github.com>
2023-04-08 14:35:45 +0800

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,7 +1,7 @@
 import private
 import requests
 
-url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttKey}?value1=31c&value2=51'
+url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttkey}?value1=31c&value2=51'
 
 r = requests.get(url)
 if r.status_code == 200:
