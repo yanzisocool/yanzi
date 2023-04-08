a92b1e40819ca45d481cf95d62d0eb6f1bed31df
yanzisocool <130196378+yanzisocool@users.noreply.github.com>
2023-04-08 14:59:00 +0800

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -6,3 +6,26 @@ url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttkey}
 r = requests.get(url)
 if r.status_code == 200:
     print("發送成功")
+
+from gpiozero import Button
+from signal import pause
+from gpiozero import RGBLED
+
+state = False
+
+def user_press():
+    global state
+    state = not state
+    if state == True:
+        print("開燈")
+        led.color=(1,0,0)
+    else:
+        print("關燈")
+        led.color=(0,0,0)
+
+button = Button(18)
+led = RGBLED(red=17, green=27, blue=22)
+
+button.when_pressed = user_press
+
+pause()
